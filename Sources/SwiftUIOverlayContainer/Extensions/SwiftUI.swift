17df54c9ba566a81918956780b408d1b3ab1db41
Yang Xu <xuyang@me.com>
2022-03-07 16:45:00 +0800

    add some extensions for Foundation and SwiftUI

---
+++ Sources/SwiftUIOverlayContainer/Extensions/SwiftUI.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUI.swift
+//
+//
+//  Created by Yang Xu on 2022/3/7
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func eraseToAnyView() -> AnyView {
+        AnyView(self)
+    }
+}
