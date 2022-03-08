92b02284725461cac84c557ee7f24188e0314745
Yang Xu <xuyang@me.com>
2022-03-08 09:30:33 +0800

    implement container type (view display type, view queue type)

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIOverlayContainer",
     platforms: [.iOS(.v15),
-                .macOS(.v12)
-    ],
+                .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIOverlayContainer",
-            targets: ["SwiftUIOverlayContainer"]),
+            targets: ["SwiftUIOverlayContainer"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIOverlayContainer",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwiftUIOverlayContainerTests",
-            dependencies: ["SwiftUIOverlayContainer"]),
+            dependencies: ["SwiftUIOverlayContainer"]
+        )
     ]
 )
