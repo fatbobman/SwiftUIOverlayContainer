b15aaf002f82a4e6d9526816731e94018f918333
Yang Xu <xuyang@me.com>
2022-03-11 18:42:45 +0800

    add more tests for queue handler

---
+++ Tests/SwiftUIOverlayContainerTests/QueueHandlerForMultipleTests.swift
@@ -0,0 +1,199 @@
+//
+//  QueueHandlerForMultipleUnitTests.swift
+//  SwiftUIOverlayContainerTests
+//
+//  Created by Yang Xu on 2022/3/11
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+import SwiftUI
+@testable import SwiftUIOverlayContainer
+import XCTest
+
+class QueueHandlerForMultipleUnitTests: XCTestCase {
+    let manager = ContainerManager.shared
+    var containerConfiguration: ContainerConfiguration!
+    var handler: ContainerQueueHandler!
+
+    override class func setUp() {
+        ContainerManager.shared.publishers.removeAll()
+    }
+
+    override class func tearDown() {
+        ContainerManager.shared.publishers.removeAll()
+    }
+
+    override func setUp() {
+        self.containerConfiguration = ContainerConfiguration(
+            displayType: .stacking, queueType: .multiple
+        )
+        self.handler = ContainerQueueHandler(
+            container: "testContainer",
+            containerConfiguration: containerConfiguration,
+            containerManager: manager,
+            emptyQueueAfterDisappear: true
+        )
+    }
+
+    override func tearDown() {
+        self.containerConfiguration = nil
+        self.handler = nil
+    }
+
+    func testShowView() throws {
+        // given
+        let view1 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+        let view2 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+
+        let perform = handler.handlerStrategy(for: .multiple)
+
+        // when
+        perform(.show(view1))
+        perform(.show(view2))
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 2)
+        XCTAssertEqual(handler.tempQueue.count, 0)
+    }
+
+    func testDismissView() throws {
+        // given
+        let view1 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+        let view2 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+
+        let perform = handler.handlerStrategy(for: .multiple)
+
+        // when
+        perform(.show(view1))
+        perform(.show(view2))
+
+        // dismiss view
+        perform(.dismiss(view1.id, true))
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 1)
+    }
+
+    func testDismissAllView() throws {
+        // given
+        let view1 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+        let view2 = IdentifiableContainerView(
+            id: UUID(), view: MessageView(), viewConfiguration: MessageView(), isPresented: nil
+        )
+
+        let perform = handler.handlerStrategy(for: .multiple)
+
+        // when
+        perform(.show(view1))
+        perform(.show(view2))
+
+        // dismiss view
+        perform(.dismissAll(false))
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 0)
+    }
+
+    func testShowViewAfterConnect() throws {
+        // given
+        let view1 = MessageView()
+        let view2 = MessageView()
+
+        handler.connect()
+
+        // when
+        let containerName = handler.container
+        manager.show(view: view1, in: containerName, using: view1)
+        manager.show(view: view2, in: containerName, using: view2)
+
+        // then
+        XCTAssertEqual(manager.containerCount, 1)
+        XCTAssertEqual(handler.mainQueue.count, 2)
+        XCTAssertEqual(handler.tempQueue.count, 0)
+
+        addTeardownBlock {
+            self.handler.disconnect()
+            XCTAssertEqual(self.manager.containerCount, 0)
+        }
+    }
+
+    func testDismissViewAfterConnect() throws {
+        // given
+        let view1 = MessageView()
+        let view2 = MessageView()
+
+        handler.connect()
+        let containerName = handler.container
+
+        // when
+        manager.show(view: view1, in: containerName, using: view1)
+        let id = manager.show(view: view2, in: containerName, using: view2)
+        manager.dismiss(view: try XCTUnwrap(id), in: containerName, animated: false)
+
+        // then
+        XCTAssertEqual(manager.containerCount, 1)
+        XCTAssertEqual(handler.mainQueue.count, 1)
+        XCTAssertEqual(handler.tempQueue.count, 0)
+
+        addTeardownBlock {
+            self.handler.disconnect()
+        }
+    }
+
+    func testDismissAllViewAfterConnect() throws {
+        // given
+        let view1 = MessageView()
+
+        handler.connect()
+        let containerName = handler.container
+
+        // when
+        manager.show(view: view1, in: containerName, using: view1)
+        manager.show(view: view1, in: containerName, using: view1)
+        manager.show(view: view1, in: containerName, using: view1)
+        manager.dismissAllView(in: [containerName], animated: false)
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 0)
+        XCTAssertEqual(handler.tempQueue.count, 0)
+
+        addTeardownBlock {
+            self.handler.disconnect()
+        }
+    }
+
+    func testDismissViewWithIsPresentedAfterConnected() throws {
+        // given
+        let source = BindingMock()
+        source.isPresented = true
+        let binding = Binding<Bool>(get: { source.isPresented }, set: { source.isPresented = $0 })
+        let view = MessageView()
+        let identifiableView = IdentifiableContainerView(id: UUID(), view: view, viewConfiguration: view, isPresented: binding)
+        let perform = handler.handlerStrategy(for: .multiple)
+
+        // when
+        perform(.show(identifiableView))
+        perform(.dismissAll(true))
+
+        // then
+        XCTAssertFalse(source.isPresented)
+    }
+}
+
+struct ContainerConfiguration: ContainerConfigurationProtocol {
+    var displayType: ContainerViewDisplayType
+    var queueType: ContainerViewQueueType
+}
---
+++ Tests/SwiftUIOverlayContainerTests/QueueHandlerTests.swift
@@ -0,0 +1,130 @@
+//
+//  QueueHandlerTests.swift
+//  SwiftUIOverlayContainerTests
+//
+//  Created by Yang Xu on 2022/3/11
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+@testable import SwiftUIOverlayContainer
+import XCTest
+
+class QueueHandlerTests: XCTestCase {
+    let manager = ContainerManager.shared
+    var containerConfiguration: ContainerConfiguration!
+    var handler: ContainerQueueHandler!
+
+    override class func setUp() {
+        ContainerManager.shared.publishers.removeAll()
+    }
+
+    override class func tearDown() {
+        ContainerManager.shared.publishers.removeAll()
+    }
+
+    override func setUp() {
+        self.containerConfiguration = ContainerConfiguration(
+            displayType: .stacking, queueType: .multiple
+        )
+        self.handler = ContainerQueueHandler(
+            container: "testContainer",
+            containerConfiguration: containerConfiguration,
+            containerManager: manager,
+            emptyQueueAfterDisappear: true
+        )
+    }
+
+    override func tearDown() {
+        self.containerConfiguration = nil
+        self.handler = nil
+    }
+
+    func testDismiss() throws {
+        // given
+        let view = MessageView()
+        let identifiableView = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+        handler.mainQueue.push(identifiableView, with: nil)
+
+        // when
+        handler.dismiss(id: identifiableView.id, animated: false)
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 0)
+    }
+
+    func testDismissAll() throws {
+        // given
+        let view = MessageView()
+        let identifiableView1 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+        let identifiableView2 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+        handler.mainQueue.push(identifiableView1, with: nil)
+        handler.mainQueue.push(identifiableView2, with: nil)
+
+        // when
+        handler.dismissAll(animated: false)
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 0)
+    }
+
+    func testPushViewIntoQueue() throws {
+        // given
+        let view = MessageView()
+        let identifiableView1 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+        let identifiableView2 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+
+        // when
+        handler.pushViewIntoQueue(identifiableView1, queue: .main)
+        handler.pushViewIntoQueue(identifiableView2, queue: .temporary)
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 1)
+        XCTAssertEqual(handler.tempQueue.count, 1)
+    }
+
+    func testDismissIfNeeded() throws {
+        // given
+        let view = MessageView()
+        let identifiableView1 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+
+        // when
+        handler.pushViewIntoQueue(identifiableView1, queue: .main)
+        handler.dismissIfNeeded()
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 0)
+    }
+
+    func testTransferNewViewFromTempQueueIfNeeded() throws {
+        // given
+        let view = MessageView()
+        let identifiableView1 = IdentifiableContainerView(
+            id: UUID(), view: view, viewConfiguration: view, isPresented: nil
+        )
+
+        handler.pushViewIntoQueue(identifiableView1, queue: .temporary)
+
+        // when
+        handler.transferNewViewFromTempQueueIfNeeded()
+
+        // then
+        XCTAssertEqual(handler.mainQueue.count, 1)
+        XCTAssertEqual(handler.tempQueue.count, 0)
+        XCTAssertEqual(handler.mainQueue.first?.id, identifiableView1.id)
+    }
+}
