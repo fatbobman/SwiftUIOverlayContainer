2c6e09931cfea35656c810b8a5f59ac0bdae66b7
Yang Xu <xuyang@me.com>
2022-03-12 16:11:45 +0800

    add some condition extension to SwiftUI

--- Sources/SwiftUIOverlayContainer/Extensions/SwiftUI.swift
+++ Sources/SwiftUIOverlayContainer/Extensions/SwiftUI.swift
@@ -28,3 +28,37 @@ public func disabledWithAnimation(_ animation: Animation? = .default, _ body: ()
         }
     }
 }
+
+// some extension for condition
+extension View {
+    @ViewBuilder
+    func `if`<V: View>(_ condition: @autoclosure () -> Bool, return result: (Self) -> V) -> some View {
+        if condition() {
+            result(self)
+        } else {
+            self
+        }
+    }
+
+    @ViewBuilder
+    func `if`<V1: View, V2: View>(
+        _ condition: @autoclosure () -> Bool,
+        return result: (Self) -> V1,
+        else elseResult: (Self) -> V2
+    ) -> some View {
+        if condition() {
+            result(self)
+        } else {
+            elseResult(self)
+        }
+    }
+
+    @ViewBuilder
+    func ifNotNil<T, V>(_ value: T?, @ViewBuilder perform action: (Self, T) -> V) -> some View where V: View {
+        if let value = value {
+            action(self, value)
+        } else {
+            self
+        }
+    }
+}
