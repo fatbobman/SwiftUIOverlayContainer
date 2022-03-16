fa87482d07ce4b2667d00263c1034ee22f6de093
Yang Xu <xuyang@me.com>
2022-03-16 18:34:04 +0800

    queue type demo

---
+++ Demo/Shared/Common/demonstrate.swift
@@ -0,0 +1,14 @@
+//
+//  demonstrate.swift
+//  Demo (iOS)
+//
+//  Created by Yang Xu on 2022/3/15
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+import Foundation
+
+enum Demonstrate {}
---
+++ Demo/Shared/Views/MessageView.swift
@@ -0,0 +1,56 @@
+//
+//  MessageView.swift
+//  Demo (iOS)
+//
+//  Created by Yang Xu on 2022/3/16
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+import SwiftUI
+import SwiftUIOverlayContainer
+
+struct Message<S: ShapeStyle>: View {
+    init(height: CGFloat, background: S, text: String, textColor: Color) {
+        self.height = height
+        self.background = background
+        self.text = text
+        self.textColor = textColor
+    }
+
+    let height: CGFloat
+    let background: S
+    let text: String
+    let textColor: Color
+
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.regularMaterial)
+            .frame(maxWidth: 400)
+            .frame(height:height)
+            .padding(.horizontal, 20)
+            .overlay(
+                Text(text)
+                    .foregroundColor(textColor)
+            )
+    }
+}
+
+extension Message:ContainerViewConfigurationProtocol {
+    var transition: AnyTransition? {
+        .move(edge: .bottom).combined(with: .opacity)
+    }
+
+    var dismissGesture: ContainerViewDismissGesture? {
+        .tap
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        Message(height: 50, background: .regularMaterial, text: "Hello world", textColor: .blue)
+            .previewLayout(.sizeThatFits)
+    }
+}
