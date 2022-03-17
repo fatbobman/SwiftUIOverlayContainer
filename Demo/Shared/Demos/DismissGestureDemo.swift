4680aa5838e8778b0a3ebc942307b0823deef59e
Yang Xu <xuyang@me.com>
2022-03-17 11:53:43 +0800

    dismiss gesture demo

---
+++ Demo/Shared/Demos/DismissGestureDemo.swift
@@ -0,0 +1,96 @@
+//
+//  DismissGestureDemo.swift
+//  Demo
+//
+//  Created by Yang Xu on 2022/3/17
+//  Copyright © 2022 Yang Xu. All rights reserved.
+//
+//  Follow me on Twitter: @fatbobman
+//  My Blog: https://www.fatbobman.com
+//
+
+import SwiftUI
+import SwiftUIOverlayContainer
+
+struct DismissGestureDemo: View {
+    @Environment(\.overlayContainerManager) var manager
+    var body: some View {
+        ZStack {
+            Color(backgroundColorOfZStack)
+            VStack {
+                Form {
+                    ForEach(gestures) { dismissGesture in
+                        Button(action: {
+                            let configuration = GestureViewConfiguration(dismissGesture: dismissGesture.gesture)
+                            let view = BlockView(size: .init(width: 200, height: 80), text: dismissGesture.description, opacity: 0.7,allowDismiss: false)
+                            manager.show(view: view, in: "gestureContainer", using: configuration)
+                        }, label: {
+                            Text(dismissGesture.description)
+                        })
+                    }
+                }
+                #if os(macOS)
+                .padding(.top,50)
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                #endif
+                ZStack{
+                    Color.clear
+                }
+                .overlayContainer("gestureContainer", containerConfiguration: GestureContainer())
+                VStack {
+                    Text("DismissGestureDescription")
+                }
+                .padding(.init(top: 0, leading: 20, bottom: 40, trailing: 20))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .navigationTitle("DismissGestureLabel")
+        #if !os(macOS)
+            .navigationBarTitleDisplayMode(.inline)
+        #endif
+    }
+}
+
+extension DismissGestureDemo {
+    var gestures: [DismissGesture] {
+        [
+            .init(gesture: .tap, description: "TapGesture"),
+            .init(gesture: .doubleTap, description: "DoubleTapGesture"),
+            .init(gesture: .longPress(0.5), description: "LongPressGesture"),
+            .init(gesture: .swipeUp, description: "SwipeUpGesture"),
+            .init(gesture: .swipeLeft, description: "SwipeLeftGesture"),
+            .init(gesture: .swipeRight, description: "SwipeRightGesture"),
+            .init(gesture: .swipeDown, description: "SwipeDownGesture")
+        ]
+    }
+}
+
+struct DismissGesture: Identifiable {
+    let id = UUID()
+    let description: LocalizedStringKey
+    let gesture: ContainerViewDismissGesture
+
+    init(gesture: ContainerViewDismissGesture, description: LocalizedStringKey) {
+        self.gesture = gesture
+        self.description = description
+    }
+}
+
+struct DismissGestureDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        DismissGestureDemo()
+    }
+}
+
+struct GestureContainer: ContainerConfigurationProtocol {
+    var displayType: ContainerViewDisplayType = .stacking
+    var queueType: ContainerViewQueueType = .oneByOne
+    var alignment: Alignment? = .center
+    var transition: AnyTransition? = .slide
+    var animation: Animation? = .default
+}
+
+struct GestureViewConfiguration: ContainerViewConfigurationProtocol {
+    var dismissGesture: ContainerViewDismissGesture?
+}
--- Demo/Shared/Views/BlockView.swift
+++ Demo/Shared/Views/BlockView.swift
@@ -12,10 +12,18 @@
 import SwiftUI
 
 struct BlockView: View {
+    init(size: CGSize, text: LocalizedStringKey, opacity: Double, allowDismiss: Bool = true) {
+        self.size = size
+        self.text = text
+        self.opacity = opacity
+        self.allowDismiss = allowDismiss
+    }
+
     let backgroundColor: Color = [.red, .orange, .pink, .blue, .green, .brown].randomElement() ?? .yellow
     let size: CGSize
     let text: LocalizedStringKey
     let opacity: Double
+    let allowDismiss: Bool
     @Environment(\.overlayContainer) var container
     var body: some View {
         Group {
@@ -26,8 +34,11 @@ struct BlockView: View {
                     Text(text)
                         .font(.callout)
                 )
-        }.onTapGesture {
-            container.dismiss()
+        }
+        .if(allowDismiss) {
+            $0.onTapGesture {
+                container.dismiss()
+            }
         }
     }
 }
@@ -36,5 +47,16 @@ struct BlockView_Previews: PreviewProvider {
     static var previews: some View {
         BlockView(size: .init(width: 70, height: 70), text: "Hello", opacity: 0.5)
             .previewLayout(.sizeThatFits)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func `if`<V: View>(_ condition: @autoclosure () -> Bool, return result: (Self) -> V) -> some View {
+        if condition() {
+            result(self)
+        } else {
+            self
+        }
     }
 }
